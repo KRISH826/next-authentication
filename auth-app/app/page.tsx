6b4bb5ef597f727028d0fb28823b7b3fc8b6eeac
KRISH826 <krishnendupanj@gmail.com>
2024-04-06 18:39:46 +0530

    hello world done (code setup)

---
+++ auth-app/app/page.tsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Button>Hello World</Button>
+    </>
+  );
+}
